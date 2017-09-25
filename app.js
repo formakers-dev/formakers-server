68c6b208787248a8798ed0ca21c7f963c40ddd82
tiffany <yoonyoungkim89@gmail.com>
2017-09-25 14:46:15 +0900

    test

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var server = app.listen(port, function () {
     console.log('Express server has started on port ', port);
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send('Hello AppBee-dragon')
 });
 
