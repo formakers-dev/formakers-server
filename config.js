c4bb31c8f0c43f2e0c551d3a6735e0965c5de271
Jason Ryu and Tiffany Kim <pair+sryu99+yoonyoungkim89@gmail.com>
2017-10-18 15:53:36 +0900

    message push api 추가 gitignore에 npm-debug.log 추가 heroku appname 변경

--- config.js
+++ config.js
@@ -4,6 +4,9 @@ module.exports = {
         port: process.env.PORT || 8080,
         google_client_id: process.env.GOOGLE_CLIENT_ID,
         google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        firebase: {
+            serverKey: process.env.SERVER_KEY
+        }
     },
     test: {
         dbUrl: process.env.MONGO_URL,
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var axios = require('axios');
 var Users = require('../models/users');
 var Apps = require('../models/apps');
+var config = require('../config')[process.env.NODE_ENV];
 
 module.exports = function (app) {
 
@@ -23,4 +25,33 @@ module.exports = function (app) {
             res.json(result);
         })
     });
+
+    app.post('/message', function (req, res) {
+        var key = config.firebase.serverKey;
+        var registrationIds = [
+            "fI2-FeMO4mA:APA91bHKGTj9es1TJAt34__LEsrLI14jauylVbL8H-hji-TxWbHo5WOiRB7RweTmG4tDcuPsiJXPDK-qh-vxe5Zz-Qlcj4A07Y_kUrpPW1O4CSwaaXUMoQBFezDffaDq7wKmkdJOjuUV"
+        ];
+        var notification = {
+            'title': '[AppBee] 앱비에서 새로운 테스트가 도착했어요',
+            'body': '테스트를 한번 확인해 보세요.',
+        };
+        var data = JSON.stringify({
+            'notification': notification,
+            'registration_ids': registrationIds
+        });
+
+        axios.post('https://fcm.googleapis.com/fcm/send', data, {
+            headers: {
+                'Authorization': 'key=' + key,
+                'Content-Type': 'application/json'
+            }
+        }).then(function (response) {
+            console.log(response);
+            res.json(true);
+        }).catch(function (error) {
+            console.error(error);
+            res.json(false);
+        });
+    });
+
 };
