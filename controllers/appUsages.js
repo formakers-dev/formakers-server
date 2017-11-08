52dcb81607e196deb548d6042aee5b9ac399dc5a
Steve Jeong and Tiffany Kim <pair+optyyie+yoonyoungkim89@gmail.com>
2017-11-09 08:49:29 +0900

    es6 문법 사용하여 getRegistrationIds, getUserIdsByPackageNames 수정

--- controllers/appUsages.js
+++ controllers/appUsages.js
@@ -1,15 +1,11 @@
 const AppUsages = require('../models/appUsages');
 
 const getUserIdsByPackageNames = (packageNameList) => {
-    return AppUsages.find({ packageName : {$in: packageNameList} }).then(apps => {
-        const result = new Set();
-        apps.forEach(app => {
-            app.users.forEach(user => {
-                result.add(user.userId);
-            });
-        });
-        return Array.from(result);
+    return AppUsages.find({packageName: {$in: packageNameList}}).then(apps => {
+        const result = [];
+        apps.forEach(app => result.push(...app.users.map(user => user.userId)));
+        return [...new Set(result)];
     });
 };
 
-module.exports = { getUserIdsByPackageNames };
+module.exports = {getUserIdsByPackageNames};
--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,11 @@
 const Users = require('../models/users');
 
-const getRegistrationIds  = (userList) => {
+const getRegistrationIds = (userList) => {
     return Users.find({userId: {$in: userList}}).then(users => {
         const result = [];
-        users.forEach(user => {
-            result.push(user.registrationToken)
-        });
+        users.forEach(user => result.push(user.registrationToken));
         return result;
     });
 };
 
-module.exports = { getRegistrationIds };
+module.exports = {getRegistrationIds};
