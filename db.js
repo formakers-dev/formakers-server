36014137cd12a7db1fbfa2f920c75ce62d4c50f8
Junghwa Yang <rosie.junghwa@gmail.com>
2020-10-07 00:55:08 +1100

    chore: DeprecationWarning 반영

--- db.js
+++ db.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose');
 const config = require('./config');
 
 const connect = () => {
-    const connection = mongoose.connect(config.dbUrl, function(err) {
-        if (err) {
-            console.error('mongodb connection error', err);
-        } else {
-            console.log('mongodb connected');
-        }
-    });
+  const connection = mongoose.connect(config.dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }, function(err) {
+    if (err) {
+      console.error('mongodb connection error', err);
+    } else {
+      console.log('mongodb connected');
+    }
+  });
 };
 
 const setRecoverConfig = () => {
