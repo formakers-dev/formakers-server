1b4bc71d49ad71109819893d00aef3a55cea1725
Rosie Yang <rosie.yang@formakers.net>
2020-10-20 23:12:05 +1100

    chore: 이메일 validation regex 수정
    https://trello.com/c/Vvv18An2

--- models/plan-b-requests.js
+++ models/plan-b-requests.js
@@ -10,7 +10,7 @@ const requestSchema = new mongoose.Schema({
 		type: String,
 		required: [true, '연락받으실 이메일 주소를 입력해주세요.'],
 		trim: true,
-		validate: [/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i, '유효한 이메일 주소를 입력해주세요.']
+		validate: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, '유효한 이메일 주소를 입력해주세요.']
 	},
 	interests: [String],
 	selectedUsers: Array,
