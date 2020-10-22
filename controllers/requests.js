2eb10f186a656e8bd9205760b713d651d8f0e6fb
Rosie Yang <rosie.yang@formakers.net>
2020-10-22 12:42:21 +1100

    feat: request 생성시 slack에 알림보내기
    https://trello.com/c/Vvv18An2

--- controllers/requests.js
+++ controllers/requests.js
@@ -1,11 +1,23 @@
 const asyncHandler = require('../middlewares/asyncHandler');
 const Request = require('../models/plan-b-requests');
 
+const { WebClient } = require('@slack/web-api');
+const config = require('../config');
+
 // @desc       Create request for user matching
 // @route      POST /requests
 // @access     Public
 exports.createRequest = asyncHandler(async(req, res, next) => {
 	const request = await Request.create(req.body);
+
+	const botApiToken = config.slackApiToken;
+	const web = new WebClient(botApiToken);
+
+	web.chat.postMessage({
+		text: `Plan B 사이트에 새로운 요청이 들어왔습니다!! 🎉 (From ${request.customerName})`,
+		channel: '_general',
+		as_user: true
+	});
 
 	res.status(201).json(request);
 });
