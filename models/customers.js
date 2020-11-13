cac41c9bb673f00f84cf470ae1f072296cc0f1e1
Rosie Yang <rosie.yang@formakers.net>
2020-11-13 21:09:58 +0900

    feat: 비밀번호 관련된 pre hook과 method 생성
    - 저장 전에 비밀번호 해싱
    - 입력된 비밀번호와 저장된 비밀번호 대조
    
    https://trello.com/c/5CCcJiik

--- models/customers.js
+++ models/customers.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const customerSchema = new mongoose.Schema({
   email: {
@@ -16,12 +17,27 @@ const customerSchema = new mongoose.Schema({
   },
   companyName: {
     type: String,
+    required: true,
     trim: true
   },
   signUpDate: {
     type: Date,
-    default: Date.now()
+    default: Date.now
   }
 });
+
+// DB에 저장하기 전에 비밀번호 해싱하기
+customerSchema.pre('save', async function (next) {
+  // 비밀번호가 변경된 경우에만 실행
+  if (!this.isModified('password')) next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// 비밀번호 대조하기
+customerSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+}
 
 module.exports = mongoose.model('customers', customerSchema);
