c23e2f1068b58da6d142a1c12ee2acdc5e5f4cca
Rosie Yang <rosie.yang@formakers.net>
2020-11-13 15:29:49 +0900

    feat: customers (게임사) DB 컬렉션 생성
    https://trello.com/c/5CCcJiik

--- models/customers.js
+++ models/customers.js
@@ -1,13 +1,27 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const customersSchema = new Schema({
-    id: String,
-    provider: String,
-    name: String,
-    email: String,
-    providerId: String,
-    verified: Boolean
+const customerSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    validate: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, '유효한 이메일 주소를 입력해주세요.']
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  companyName: {
+    type: String,
+    trim: true
+  },
+  signUpDate: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
-module.exports = mongoose.model('customers', customersSchema);
+module.exports = mongoose.model('customers', customerSchema);
